cb306d76b7bfa65497f2d4524c1fb4da0fd05442
LeeFreud <lijunxian999@gmail.com>
2018-06-22 07:37:30 +0800

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-var header = document.getElementById("0");
-var nav = document.getElementById("1");
-var article = document.getElementById("2");
-header.innerHTML = "title here. ";
-nav.innerHTML = "nav here. ";
-article.innerHTML = "article here. ";
